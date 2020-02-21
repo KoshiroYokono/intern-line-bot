470e350599814c46e31a18de53274387fc37e0ce
KoshiroYokono <katsuo.ksr116@gmail.com>
2020-02-21 15:29:06 +0900

    不要メソッドの削除

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -3,13 +3,6 @@ require 'line/bot'
 class WebhookController < ApplicationController
   protect_from_forgery except: [:callback] # CSRF対策無効化
 
-  def client
-    @client ||= Line::Bot::Client.new { |config|
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-    }
-  end
-
   def callback
     body = request.body.read
     signature = request.env['HTTP_X_LINE_SIGNATURE']
